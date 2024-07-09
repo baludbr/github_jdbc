91594684e0bc6ccade71c730a3bac32ff6f1cc40
balubbr <dwarampudibalajireddy@gmail.com>
2024-07-09 22:56:12 +0530

    add

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,10 @@
-#
 # Build stage
-#
-FROM maven:3.8.2-jdk-11 AS build
+FROM maven:3.8.2-openjdk-17 AS build
 COPY . .
 RUN mvn clean package -DskipTests
 
-#
 # Package stage
-#
-FROM openjdk:11-jdk-slim
-COPY --from=build /target/Project_1-0.0.1-SNAPSHOT.jar 
-# ENV PORT=8080
+FROM openjdk:17-jdk-slim
+COPY --from=build /target/Project_1-0.0.1-SNAPSHOT.jar /app/Project_1-0.0.1-SNAPSHOT.jar
 EXPOSE 8080
-ENTRYPOINT ["java","-jar"]
+ENTRYPOINT ["java", "-jar", "/app/Project_1-0.0.1-SNAPSHOT.jar"]
--- src/main/java/Project/Project_1/App.java
+++ src/main/java/Project/Project_1/App.java
@@ -1,13 +1,36 @@
 package Project.Project_1;
 
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws LifecycleException
     {
-        System.out.println( "Hello World!" );
+        Tomcat tomcat = new Tomcat();
+
+        // Set the port number
+        tomcat.setPort(8081);
+
+        // Define the web application context
+        String contextPath = "/Project_1";
+        String docBase = new File("src/main/webapp").getAbsolutePath();
+
+        // Add the web application context to the Tomcat server
+        Context context = tomcat.addContext(contextPath, docBase);
+
+        // Ensure JSP support is configured
+        context.setDocBase(docBase);
+
+        // Start the Tomcat server
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
